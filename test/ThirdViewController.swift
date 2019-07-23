00fbfa9753059f3f4bdb497c67812808b3e0f44e
visitor2948 <rucko777@gmai.com>
2019-07-23 12:48:50 -0500

    something

--- test/ThirdViewController.swift
+++ test/ThirdViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 import SQLite3
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var listView2: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
     
-    @IBOutlet weak var listView: UITableView!
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        return cell
+    }
+    
     let dbPath = "./Recepies.db"
     
     func openDatabase() -> OpaquePointer? {
@@ -47,33 +55,60 @@ class ThirdViewController: UIViewController {
                 }  else {
                     print("name not found")
                 }
+            let servings = sqlite3_column_int64(statement, 0)
+            print ("servings = \(servings); ", terminator: "")
+            
+        }
+        if let cString = sqlite3_column_text(statement, 1) {
+            let serving_size = String(cString: cString)
+            print("serving_size = \(serving_size)")
+            
+            if let cString = sqlite3_column_text(statement, 1) {
+                let src = String(cString: cString)
+                print("src = \(src)")
+                
+                let id = sqlite3_column_int64(statement, 0)
+                print ("id = \(id); ", terminator: "")
+                
+                let recipeID = sqlite3_column_int64(statement, 0)
+                print ("recipeID = \(recipeID); ", terminator: "")
+    
+                let step_number = sqlite3_column_int64(statement, 0)
+                print ("step_number = \(step_number); ", terminator: "")
+                
+                let step = String(cString: cString)
+                print("step = \(step)")
+                
+                let type = String(cString: cString)
+                print("type = \(type)")
+                
+                let amount = String(cString: cString)
+                print("amount = \(amount)")
+                
+                let ingredient = String(cString: cString)
+                print("ingredient = \(ingredient)")
+                
+                let description = String(cString: cString)
+                print("description = \(description)")
+                
+                if sqlite3_finalize(statement) != SQLITE_OK {
+                    let errmsg = String(cString: sqlite3_errmsg(db)!)
+                    print("error finalizing prepared statement: \(errmsg)")
+                }
+                statement = nil
             }
+        let queryStatementString = "SELECT * FROM Contact;"
+        let insertStatementString = "INSERT INTO Contact (Id, Name) VALUES (?, ?);"
+        let queryStatementString2 = "SELECT * FROM (file name);"
     
-    if sqlite3_finalize(statement) != SQLITE_OK {
-        let errmsg = String(cString: sqlite3_errmsg(db)!)
-        print("error finalizing prepared statement: \(errmsg)")
+            func tableView( _ tableView: UITableView, numberOfRowsInSection section: Int)
+                -> Int 
+        }
     }
-    statement = nil
-}
-    let queryStatementString = "SELECT * FROM Contact;"
-    let insertStatementString = "INSERT INTO Contact (Id, Name) VALUES (?, ?);"
-    let queryStatementString2 = "SELECT * FROM (file name);"
-    
-        override func viewDidLoad() {
-            super.viewDidLoad()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
